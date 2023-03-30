2c923f6c47b7bd848925ee74b34ad63454a6b5c7
Junio Sousa <juniosousa120@gmail.com>
2023-03-30 01:39:26 -0300

    End Project

--- script.js
+++ script.js
@@ -8,25 +8,57 @@ const resultado = document.querySelector(".resultado");
 
 let array = [];
 
-const boobleSort = () => {
-  console.log(array);
+const bubbleSort = () => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        let aux = array[i];
+        array[i] = array[j];
+        array[j] = aux;
+      }
+    }
+  }
+  resultado.innerText = array;
+};
+
+const selectionSort = () => {
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    let aux;
+    for (let j = i + 1; j <= array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (i != min) {
+      aux = array[i];
+      array[i] = array[min];
+      array[min] = aux;
+    }
+  }
+  resultado.innerText = array;
 };
 
 const insertionSort = () => {
-  console.log(array);
-};
-
-const selectSort = () => {
-  console.log(array);
+  for (let i = 1; i < array.length; i++) {
+    let current = array[i];
+    let j = i - 1;
+    while (j > -1 && current < array[j]) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = current;
+  }
+  resultado.innerText = array;
 };
 
 const checkdButton = array => {
   btn1.removeAttribute("disabled");
   btn2.removeAttribute("disabled");
   btn3.removeAttribute("disabled");
-  if (array.length > 5 && array.length <= 10) {
+  if (array.length > 20 && array.length <= 50) {
     btn1.setAttribute("disabled", "");
-  } else if (array.length > 10 && array.length <= 15) {
+  } else if (array.length > 50 && array.length <= 100) {
     btn1.setAttribute("disabled", "");
     btn2.setAttribute("disabled", "");
   }
@@ -34,13 +66,12 @@ const checkdButton = array => {
 
 const chnageValores = () => {
   const valor = entrada.value;
-  resultado.innerText = valor;
   array = valor.split(" ").map(Number);
   checkdButton(array);
 };
 
 entrada.addEventListener("change", chnageValores);
 
-btn1.addEventListener("click", boobleSort);
-btn2.addEventListener("click", insertionSort);
-btn3.addEventListener("click", selectSort);
+btn1.addEventListener("click", bubbleSort);
+btn2.addEventListener("click", selectionSort);
+btn3.addEventListener("click", insertionSort);
